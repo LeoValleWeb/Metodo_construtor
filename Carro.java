2e49092050ec27820167b18bf744619a4ebf9b8b
LeoValleWeb <leomartinsvalle@gmail.com>
2023-10-24 13:43:18 +0000

    save

---
+++ Carro.java
@@ -0,0 +1,32 @@
+public class Carro{
+    private String cor;
+    private String modelo;
+    private int aro;
+    private double valor;
+
+
+    public Carro(){
+
+    }
+    public Carro(int aro){
+        this.aro=aro;
+    }
+    public Carro(int aro, String modelo){
+        this.aro=aro;
+        this.modelo=modelo;
+    }
+    public Carro(int aro, String modelo, double valor){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valor=valor;
+    }
+    public Carro(int aro, String modelo, double valor, String cor){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valor=valor;
+        this.cor=cor;
+    }
+
+
+
+}

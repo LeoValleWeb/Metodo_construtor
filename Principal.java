52c9eb1e905f6775ce3f13c3e0b2c2bf526fa44b
LeoValleWeb <leomartinsvalle@gmail.com>
2023-10-24 14:57:04 +0000

    save

--- Principal.java
+++ Principal.java
@@ -1,6 +1,18 @@
 public class Principal {
 
-    public static void manin(String args){
+    public static void manin(String[] args){
+
+        Carro car = new Carro();
+        Carro carAro = new Carro(14);
+        Carro carParametros = new 
+        Carro(14, "HB 20 2021 sense", 60000, "Branco");
+        Carro carQuatroParametros = new Carro(17, "Civc", 100000, "Preto");
+
+        System.out.println(carAro.getAro());
+        System.out.println(carQuatroParametros.getCor());
+        System.out.println(carParametros.getValor());
+        System.out.println(car);
+
 
         
     }
